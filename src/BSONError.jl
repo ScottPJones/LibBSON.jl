7ea9bbf9c774e6541533692565917b307c83be16
Peter Zion <peter.zion@gmail.com>
2014-08-31 10:58:38 -0400

    Error domains and codes

--- src/BSONError.jl
+++ src/BSONError.jl
@@ -7,11 +7,23 @@ type BSONError
 end
 export BSONError
 
+const domainDescs = {1 => "JSON", 2 => "READER"}
+const errorDescs = {
+    1 => {
+        1 => "READ_CORRUPT_JS",
+        2 => "READ_INVALID_PARAM",
+        3 => "READ_CB_FAILURE",
+        },
+    2 => {
+        1 => "BADFD",
+        },
+    }
+
 function error(bsonError::BSONError)
     uint32s = reinterpret(Uint32, bsonError._wrap_)
     domain = uint32s[1]
     code = uint32s[2]
     message = bytestring(bsonError._wrap_[9:end])
-    error("libBSON (domain $domain, code $code): $message")
+    error("libBSON:$(domainDescs[domain]):$(errorDescs[domain][code]): $message")
 end
 export error
