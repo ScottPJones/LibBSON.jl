5f50545b7db6119a0f69e2c37992956aaea3075f
Peter Zion <peter.zion@gmail.com>
2016-09-20 12:57:20 -0400

    Fixes for Julia 0.4

--- src/BSONError.jl
+++ src/BSONError.jl
@@ -1,3 +1,6 @@
+using Compat
+import Compat.String
+
 type BSONError
     _wrap_::Vector{UInt8}
 
@@ -14,15 +17,15 @@ const errorDescs = @compat Dict{Int64,Any}(1 => (@compat Dict{Int64,AbstractStri
         3 => "READ_CB_FAILURE"
     )), 2 => @compat Dict{Int64,AbstractString}(1 => "BADFD"))
 
-convert(::Type{AbstractString}, bsonError::BSONError) = begin
+convert(::Type{String}, bsonError::BSONError) = begin
     uint32s = reinterpret(UInt32, bsonError._wrap_)
     domain = uint32s[1]
     code = uint32s[2]
-    return String(copy(convert(Ptr{UInt8}, bsonError._wrap_[9:end])))
+    return Compat.String(bsonError._wrap_[9:end])
 end
 export convert
 
-string(bsonError::BSONError) = String(copy(bsonError._wrap_))
+string(bsonError::BSONError) = String(bsonError._wrap_)
 export string
 
 error(bsonError::BSONError) = error(string(bsonError))
